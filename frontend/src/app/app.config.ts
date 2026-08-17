a7ff69d490df1eb5fe16b65971e5aef795660dfa
sandeepbhuvan <sandeepbhuvan@gmail.com>
2026-08-17 23:23:58 +0530

    feat: initialize Angular application with app configuration and standalone bootstrap logic

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withFetch()),
-    provideAnimationsAsync(),
+    provideAnimations(),
   ]
 };
